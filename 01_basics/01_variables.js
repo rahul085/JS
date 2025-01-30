1cd8e023dfd0561939b0f5a06ea11ad7bc9b4616
rahul085 <150836669+rahul085@users.noreply.github.com>
2025-01-30 14:33:51 +0000

    01_variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,22 @@
+const accountId=144553;
+let accountEmail="rahulneeli2003@gmail.com";
+var accountPassword="336655";
+accountCity="Hyderabad";
+
+//accountId=2; // not allowed
+
+accountEmail="hc@hc.com"
+accountPassword="225522";
+accountCity="Jaipur"
+let accountState;
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+
+
+/*
+Prefer not to use var to declare variables
+
+
+*/
