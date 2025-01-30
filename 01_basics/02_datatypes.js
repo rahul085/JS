e5509f7be54aaaca65bfd70779426a6c98295e1d
rahul085 <150836669+rahul085@users.noreply.github.com>
2025-01-30 15:25:19 +0000

    data types

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,16 @@
+"use strict" // treat all js code as newer version
+// alert(3+3) // we are using nodejs, not browser
+let name="rahul";
+let age= 21;
+let isLogin=false;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// null => standalone value
+// boolean => true/false
+// undefined => 
+// symbol => unique
+console.log(typeof age);
+console.log(typeof null)
+console.log(typeof undefined)
